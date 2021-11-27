7c1010e09116f53911bf19d18aa3a096a5aaa060
arn00sdq <ad.dudes@outlook.fr>
2021-11-27 11:21:40 +0100

    reagencement css

--- src/components/common/DisplayContent/summarize/rowContent.js
+++ src/components/common/DisplayContent/summarize/rowContent.js
@@ -3,10 +3,10 @@ const Row = (props) => {
     const row=[];
     if(typeof props === 'object'){
         for (const [key, value] of Object.entries(props.elt)){
-            row.push(<div key={value} className="flag-point-small">{value}</div>)
+            row.push(<div key={value} className="tag-point-small">{value}</div>)
         }
     }else{
-        row.push(<div key={props} className="level-flag">{props}</div>)
+        row.push(<div key={props} className="level-tag">{props}</div>)
     }
     return row;
 }
@@ -14,7 +14,7 @@ const Row = (props) => {
 function RowContentSummarize(props){
     return (
         <div className="row-section-summarize">
-            <div className="title-row-summarize">{props.title}</div>
+            <div className="title-row-summarize">{props.title} :</div>
             <div className="row-content-summarize">
                 {Row(props.row)}
             </div>
