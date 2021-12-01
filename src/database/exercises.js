abb867e86db1a15a43877bbae023ad3d63d78b48
iadmunio <ivan.alonso-diaz-munio@etud.univ-pau.fr>
2021-12-01 09:32:15 +0100

    Finished exercice exacmple database

--- src/database/exercises.js
+++ src/database/exercises.js
@@ -1,10 +1,12 @@
 import { _uniqueId } from "lodash/uniqueId";
-import {basket_jump, everest} from  "./images"
+import {basket_jump, everest, medium_parrot, feather} from  "./images"
+import {data as levels} from "./levels" 
+import {data as themes} from "./themes"
 
 const data = [
     {
         id,
-        type,
+        type : "Word2Img",
         level : "Débutant (A1 - A2)",
         categories :["Present-perfect","Instruments"],
         rewards : {
@@ -24,6 +26,45 @@ const data = [
                 opt: ["guitare", "flute", "acordeon"],
                 type: "picture",
             },
+        },
+    },
+    {
+        id,
+        type : "Word2Img",
+        level : levels.BEGINNER,
+        theme : themes.ANIMALS,
+        rewards : {
+            treasures : 0,
+            feathers : 2,
+        },
+        content : {
+            order: "Pick the word that best suits the image",
+            steps : 4,
+            images : 
+            [
+                {
+                    url : everest,
+                    word : 'Mountain'
+                },
+                {
+                    url : medium_parrot,
+                    word : 'Bird'
+                },
+                {
+                    url : feather,
+                    word : 'Feather'
+                },
+                {
+                    url : basket_jump,
+                    word : 'Basketball'
+                }
+            ],
+            words : [
+                ['Door', 'Hand', 'Guitar', 'Mountain'],
+                ['Dog', 'Bird', 'Spider', 'Mouse'],
+                ['Plane', 'Feather', 'Treasure', 'House'],
+                ['Football', 'Cricket', 'Basketball', 'School']
+            ],
         },
     }
 ];
---
+++ src/database/levels.js
@@ -0,0 +1,8 @@
+const data = Object.freeze({
+    BEGINNER : "Beginner (A1-A2)",
+    INTERMEDIATE : "Intermediate (B1-B2)",
+    ADVANCED : "Advanced (C1-C2)"
+  });
+  
+export default {data}
+  
