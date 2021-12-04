c06cd465266f247fa46c42a26da7909027e2a94a
iadmunio <ivan.alonso-diaz-munio@etud.univ-pau.fr>
2021-12-04 14:04:29 +0100

    changes

--- src/components/common/displayActivity/displayActivity.js
+++ src/components/common/displayActivity/displayActivity.js
@@ -8,30 +8,12 @@ import React from "react";
 
 const _ = require("lodash");
 
-<<<<<<< HEAD
 
 
 function DisplayActivity({ state, handles }) {
   var id = state.exercisePage.exerciseId;
   var step = state.exercisePage.step
   var exercises = exercisesData;
-=======
-const switchContent = (prefix, content) => {
-  switch (prefix) {
-    case "EX":
-      return <DisplayExercice exercise={content} />;
-    case "LE":
-      return <DisplayLecon lesson={content} />;
-    case "2":
-      return <DisplayPaper paper={content} />;
-      break;
-  }
-};
-
-function DisplayActivity({ id }) {
-  // affiche journaux, lesson , exercice
-  var activity = exercisesData;
->>>>>>> d13519c0dcb7b725ceb0c7c76dd78f9a4223f4cb
   var prefixType = id.substr(0, 2);
 
   const switchActivity = (prefix, activity, step) => {
@@ -59,16 +41,11 @@ function DisplayActivity({ id }) {
   console.log(currentActivity)
   return (
     <>
-<<<<<<< HEAD
       <DisplaySummary
         name={`${id} ${": "} ${currentExercise.type}`}
         summary={currentExercise.summary}
       />
       {switchActivity(prefixType, currentExercise, step)}
-=======
-      <DisplaySummary name={`${id} ${": "} ${currentActivity.type}`} summary={currentActivity.summary} />
-      {switchContent(prefixType, currentActivity)}
->>>>>>> d13519c0dcb7b725ceb0c7c76dd78f9a4223f4cb
     </>
   );
 }
