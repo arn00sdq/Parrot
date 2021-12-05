dbd95a04cf95d459bdf6b19f91e166a9496f1ddc
arn00sdq <ad.dudes@outlook.fr>
2021-12-05 11:41:24 +0100

    all route done

--- src/app.js
+++ src/app.js
@@ -12,29 +12,26 @@ import PaperPage from "./components/paper/paperPage";
 
 import "./assets/css/index.css";
 
-import { Routes, Route, useLocation} from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
-export default function App({state, handles}) {
-    console.log(state)
-    const location = useLocation();
+export default function App({ state, handles }) {
+  console.log(state)
+  const location = useLocation();
 
   React.useEffect(() => {
     console.log('Location changed');
   }, [location]);
-    return (
-            <Routes>
-                <Route path="/" element={<Layout state = {state} handles = {handles}/>}>
-                    <Route exact path="home" element={<Home state = {state} handles = {handles}/>} />
-                    <Route path="exercicePicker" element={<ExercicePicker state = {state} handles = {handles}/>} />
-                    <Route path="LessonPicker" element={<LessonPicker state = {state} handles = {handles}/>} />
-                    <Route path="paperPicker" element={<PaperPicker state = {state} handles = {handles}/>} />
-                    <Route path="exercicePage" element={<ExercicePage state = {state} handles = {handles}/>} />
-                    <Route path="lessonPage" element={<LessonPage state = {state} handles = {handles}/>} />
-                    <Route path="paperPage" element={<PaperPage state = {state} handles = {handles}/>} />
-                    
-                    {/*<Route path="lessonPicker" element={<LessonPicker state = {state} handles = {handles}/>} />
-                    <Route path="paperPage" element={<PaperPage state = {state} handles = {handles}/>} />
-    <Route path="toiecPage" element={<ToiecPage state = {state} handles = {handles}/>} />*/}
+  return (
+    <Routes>
+      <Route path="/" element={<Layout state={state} handles={handles} />}>
+        <Route exact path="home" element={<Home state={state} handles={handles} />} />
+        <Route path="exercicePicker" element={<ExercicePicker state={state} handles={handles} />} />
+        <Route path="LessonPicker" element={<LessonPicker state={state} handles={handles} />} />
+        <Route path="paperPicker" element={<PaperPicker state={state} handles={handles} />} />
+        <Route path="exercicePage" element={<ExercicePage state={state} handles={handles} />} />
+        <Route path="lessonPage" element={<LessonPage state={state} handles={handles} />} />
+        <Route path="paperPage" element={<PaperPage state={state} handles={handles} />} />
+        <Route path="toiecPage" element={<ToiecPage state={state} handles={handles} />} />
       </Route>
     </Routes>
   );
