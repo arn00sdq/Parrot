aa62e092ad301c73b3413c23832895d22ebdbf07
arn00sdq <ad.dudes@outlook.fr>
2021-12-07 21:42:04 +0100

    paperPage v1

--- src/database/paper.js
+++ src/database/paper.js
@@ -7,7 +7,7 @@ import {
     data as themes
 } from "./themes";
 
-import {ar1_img1} from '../database/images'
+import { ar1_img1 } from '../database/images'
 
 var data = [
     {
@@ -16,7 +16,7 @@ var data = [
         type: "Article",
         summary: {
             level: levels.BEGINNER,
-            time : 5,
+            time: 5,
             themes: [
                 themes.ANIMALS,
                 themes.CLOTHING,
@@ -31,15 +31,19 @@ var data = [
             text: [
                 <>
                     <p className="article-title"> Covid-19 : les laboratoires sur le pied de guerre pour adapter leur vaccin au variant Omicron</p>
-                    <p className="article-reporter-infos">Par Paul Marion et AFP agence</p>
-                    <p className="article-date">Publié le 27/11/2021 à 15:33, mis à jour le 01/12/2021 à 15:56</p>
-                    <img src={ar1_img1}></img>
-                    <p className="article-subtitle">Pfizer-BioNTech et Moderna sont prêts à mettre à jour rapidement 
-                                                    leurs vaccins actuels pour les rendre efficaces contre le variant Omicron.</p>
-                    <p className="article-text">Pfizer a déjà commencé à travailler sur une nouvelle version de son vaccin anti-Covid ciblant plus spécifiquement Omicron 
-                    au cas où le vaccin actuel ne serait pas suffisamment efficace contre le nouveau variant, a assuré lundi le PDG de Pfizer, Albert Bourla. 
-                    «Il y a encore beaucoup d'inconnues» autour du nouveau variant détecté en Afrique du Sud et jugé «préoccupant» par l'OMS, a-t-il souligné dans une interview sur la chaîne américaine CNBC. 
-                    «Nous saurons l'essentiel de ce qu'il y a à savoir d'ici quelques semaines».</p>
+                    <span className="article-reporter-infos">Par <span className="reporter">Paul Marion</span> et AFP agence<br /></span>
+                    <span className="article-date">Publié le 27/11/2021 à 15:33, mis à jour le 01/12/2021 à 15:56<br /></span>
+                    <div className="media-picture">
+                        <img className="large-ptc-article" src={ar1_img1}></img>
+                        <figcaption className="article-figcation">Des chercheurs en tenue de protection travaillent dans un laboratoire de l’Institut de virologie de Wuhan, le 6 février 2020, en Chine.
+                        <span className="fig-credit">China Daily CDIC/REUTERS</span></figcaption>
+                    </div>
+                    <div className="article-subtitle">DÉCRYPTAGE - Selon le NIH, leurs travaux sont allés au-delà 
+                                                      de ce qui avait été précédemment admis officiellement.</div>
+                    <div className="article-text">Pfizer a déjà commencé à travailler sur une nouvelle version de son vaccin anti-Covid ciblant plus spécifiquement Omicron
+                    au cas où le vaccin actuel ne serait pas suffisamment efficace contre le nouveau variant, a assuré lundi le PDG de Pfizer, Albert Bourla.
+                    «Il y a encore beaucoup d'inconnues» autour du nouveau variant détecté en Afrique du Sud et jugé «préoccupant» par l'OMS, a-t-il souligné dans une interview sur la chaîne américaine CNBC.
+                    «Nous saurons l'essentiel de ce qu'il y a à savoir d'ici quelques semaines».</div>
                 </>
 
             ],
@@ -47,6 +51,6 @@ var data = [
     }
 ];
 
-export { 
+export {
     data
 };
