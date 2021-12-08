9892cb4aa43ec967d280e175a0c97be5872b9d28
arn00sdq <ad.dudes@outlook.fr>
2021-12-08 19:17:31 +0100

    homepage css upgrade v1

--- src/components/home/dashboard/HeaderDashCard.js
+++ src/components/home/dashboard/HeaderDashCard.js
@@ -5,6 +5,8 @@ function HeaderDashCard(props){
             <div className="header-dash-text">{props.headerText}</div>
             <div className="help-container">
                 <div className="header-dash-text">Aide</div>
+                <span className="help-text">Vos trois dernières activités sont sauvegardés<br/> 
+                                            Un historique sera implenté à l'avenir dans  defaultUser</span>
                 <div className="orange-triangle-down"></div>
             </div>                   
         </div>
