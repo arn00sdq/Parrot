cb18d2522dfd9d5568aaef089de2692bc9295262
arn00sdq <ad.dudes@outlook.fr>
2021-12-11 22:51:03 +0100

    test

--- src/components/common/displayActivity/exercice/types/wordDrifting.js
+++ src/components/common/displayActivity/exercice/types/wordDrifting.js
@@ -1,9 +1,11 @@
-import React , {useState} from 'react';
+import React, { useState } from 'react';
 import { achievement, GameImg, feather_icon } from "../../../../../database/images";
 
 function WordDrifting({ content, currentStep, handles, gameInfo }) {
-    
-    const [isActive, setActive]= useState(false)
+
+    const [isActive, setActive] = useState(false);
+    const [isActiveButton, setActiveButton] = useState(false);
+    var row = [];
 
     const handleButtonClick = (e) => {
         e.preventDefault();
@@ -13,22 +15,48 @@ function WordDrifting({ content, currentStep, handles, gameInfo }) {
         } = e;
 
         if (content.theme[0].answer.includes(value)) {
-            handles.handleAddPointExercisePage();
             toggleClass();
-        }else{
+            handles.handleAddPointExercisePage();
+            renderButton();
+
+        } else {
 
         }
     };
 
     const toggleClass = () => {
         setActive(!isActive);
+
         setTimeout(() => {
             setActive(false);
-          }, 200)
+
+        }, 800)
     }
+
+    const renderButton = () => {
+        row.push(
+            <button
+                onClick={(e) => handleButtonClick(e)}
+                value={"saw"}
+                className={isActiveButton ? "active-container" : "word-vrom"}>Doggo
+        </button>
+        )
+    }
+
+    /* Donnée Joueur */
 
     var life = gameInfo.life;
     var points = gameInfo.points;
+
+    /* Defintion vague */
+
+    row.push(
+        <button
+            onClick={(e) => handleButtonClick(e)}
+            value={"saw"}
+            className={isActiveButton ? "active-container" : "word-vrom"}>Doggo
+        </button>
+    )
 
     return (
         <div className="drift-container">
@@ -39,15 +67,11 @@ function WordDrifting({ content, currentStep, handles, gameInfo }) {
 
             <div className="points-container">
                 <img className="life-icon" src={feather_icon} />
-                <span className="points-number">{points}</span>
+                <span className={isActive ? "active-point-number points-number" : "points-number"}>{points}</span>
             </div>
 
             <div className="word-container">
-                <button
-                    onClick={(e) => handleButtonClick(e)}
-                    value={"saw"}
-                    className="word-vrom">Doggo
-                </button>
+                {row}
                 <span className={isActive ? "active-points points-word" : "points-word"}>+1</span>
             </div>
             {/*<div className="infinite">
