4a723044058e4af9c13f88bd2fb2d0c158d1f373
arn00sdq <ad.dudes@outlook.fr>
2021-12-12 15:21:56 +0100

    small fix

--- src/components/common/displayActivity/common/footerContent.js
+++ src/components/common/displayActivity/common/footerContent.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { next_arrow, previous_arrow } from "../../../../database/images";
-function FooterContent({ currentStep, handles }) {
-  const handleButtonClick = (e) => {
-    e.preventDefault();
+
 
 function FooterContent({handles }) {
     const handleButtonClick = (e) => {
