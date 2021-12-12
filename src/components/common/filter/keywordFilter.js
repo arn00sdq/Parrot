a83930be64f985d1dc1046444e28aa607604af52
iadmunio <ivan.alonso-diaz-munio@etud.univ-pau.fr>
2021-12-12 12:55:21 +0100

    keyword filter

--- src/components/common/filter/keywordFilter.js
+++ src/components/common/filter/keywordFilter.js
@@ -1,19 +1,31 @@
 import React from "react";
-import {PickerImg } from "../../../database/images";
+import { PickerImg } from "../../../database/images";
 
-export default function KeywordFilter({handles, from}) {
+export default function KeywordFilter({ handles, from }) {
+  function handleinputChange(e) {
+    const input = e.target.value;
+    handles.handleInputSearchFilterChange(input, from)
+  }
   return (
     <div className="section-filter-search">
-              <p className="title-search-section">
-                <img src={PickerImg.key} className="icon-filter"/>
-                <span className="title-search">Keys Words</span>
-              </p>
-              <div className="search-bar">
-                <div className="search-form">
-                  <input name="keywords" type="text" placeholder="Research" className="search-field"></input>
-                  <button className="search-send"><img src={PickerImg.search_icon} className="icon-filter-button"/></button>
-                </div>
-              </div>
-            </div>
+      <p className="title-search-section">
+        <img src={PickerImg.key} className="icon-filter" />
+        <span className="title-search">Keys Words</span>
+      </p>
+      <div className="search-bar">
+        <div className="search-form">
+          <input
+            name="keywords"
+            type="text"
+            placeholder="Research"
+            className="search-field"
+            onChange={e => handleinputChange(e)}
+          ></input>
+          <button className="search-send">
+            <img src={PickerImg.search_icon} className="icon-filter-button" />
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
