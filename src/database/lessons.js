7155e1391ed88f7222d1af1f2c79e239ab1eaa44
arn00sdq <ad.dudes@outlook.fr>
2021-12-13 11:44:55 +0100

    lesson update not finished

--- src/database/lessons.js
+++ src/database/lessons.js
@@ -23,88 +23,222 @@ var data = [
     },
     content: {
       stepsContent: [
+        /*
+         * *
+          1er section
+            **
+              */
         <>
           <div className="title">
-
-            <div className="num-block">
-              <span className="num-section-less">1</span>
+            <div className="num-primary-block">
+              <span className="num-primary-less">I</span>
             </div>
-            <p className="title-section-less"> Formation du Present Perfect </p>
+            <p className="title-primary-less"> Formation du Present Perfect et du Present Be-ing </p>
+          </div>
+          <div className="shift-lesson">
+            <div className="title">
+              <div className="num-block">
+                <span className="num-section-less">A</span>
+              </div>
+              <p className="title-section-less"> Formation du Present Perfect </p>
+            </div>
+            <p className="subTitle">Règle de conjugaison</p>
+
+            <ul className="grammar-rule">
+              <li>
+                <p>Pour les verbes réguliers :</p>
+                <p> sujet + <b> auxiliaire have au present + base verbale  + -ed</b> </p>
+              </li>
+              <li>
+                <p>Pour les verbes irrégulier :</p>
+                <p>sujet + <b>auxiliaire have au present + base verbale  + participe passé régulier</b></p>
+              </li>
+            </ul>
+
+            <div className="exemple-section">
+              <div className="lesson-exemple">
+                <span className="title-exemple">Exemple</span>
+                <p className="text">
+                <span className="wordPrimary">Have</span>  you <span className="wordPrimary">finished</span> your homework ?
+                </p>
+                <span className="trad-exemple">Est ce que tu as finis tes devoirs</span>
+              </div>
+              <div className="lesson-exemple">
+                <span className="title-exemple">Exemple</span>
+                <p className="text">
+                  Bob <span className="wordPrimary">has has washed </span> the dishes
+                </p>
+                <span className="trad-exemple">Bob a fait la vaiselle</span>
+              </div>
+            </div>
+
+            <p className="explication">
+              Quand l'action s'est-elle passée ? Dans le passé : il y a quelques
+              minutes/hier Quelle conséquence a-t-elle sur le présent ?
+            </p>
+          </div>    
+
+        </>,
+        /*
+         * *
+          2er section
+            **
+              */
+        <>
+          <div className="title">
+            <div className="num-block">
+              <span className="num-section-less">B</span>
+            </div>
+            <p className="title-section-less"> Formation du Present Perfect Be-ing </p>
           </div>
           <p className="subTitle">Règle de conjugaison</p>
 
-          <ul className="grammar-rule">
-            <li>
-              <p>Pour les verbes réguliers :</p>
-              <p> sujet + <b> auxiliaire have au present + base verbale  + -ed</b> </p>
-            </li>
-            <li>
-              <p>Pour les verbes irrégulier :</p>
-              <p>sujet + <b>auxiliaire have au present + base verbale  + participe passé régulier</b></p>
-            </li>
-          </ul>
-
-          <div className="title">
-            <div className="num-block">
-              <span className="num-section-less">2</span>
-            </div>
-            <p className="title-section-less"> Principaux emplois </p>
-          </div>
-
-          <p className="subTitle">Actions passées qui ont des conséquences sur le présent.</p>
+          <p className="text-explanation">Le present perfect be-ing se construit à toutes les formes avec have been
+              conjugué au present perfect à la forme voulue suivi de la base verbale + -ing du verbe utilisé
+          </p>
 
           <div className="exemple-section">
             <div className="lesson-exemple">
               <span className="title-exemple">Exemple</span>
               <p className="text">
-                Paul all <span className="wordPrimary">has eaten</span> the cookies
-          </p>
-              <span className="trad-exemple">Paul a mangé tout les cookies</span>
+                Amy  <span className="wordPrimary">Has been sleeping</span>  a lot lately <span className="wordPrimary">hasn't</span> she ?
+              </p>
+              <span className="trad-exemple">Amy a beaucoup dormi ces derniers temps.</span>
             </div>
             <div className="lesson-exemple">
               <span className="title-exemple">Exemple</span>
               <p className="text">
-                Paul all <span className="wordPrimary">has eaten</span> the cookies
-          </p>
-              <span className="trad-exemple">Paul a mangé tout les cookies</span>
+                I <span className="wordPrimary"> haven't been sleeping </span> a lot lately
+              </p>
+              <span className="trad-exemple">Je n'ai pas beaucoup dormi ces derniers temps</span>
             </div>
           </div>
 
           <p className="explication">
             Quand l'action s'est-elle passée ? Dans le passé : il y a quelques
-            minutes/hier Quelle conséquence a-t-elle sur le présent ? Paul a
-            mangé tous les gâteaux ; il n'en reste plus pour moi., xd
-      </p>
-
+            minutes/hier Quelle conséquence a-t-elle sur le présent ?
+          </p>
         </>,
+        /*
+         * *
+          3er section
+            **
+              */
         <>
-          <p className="subTitle">
-            Actions qui ont commencé dans le passé et qui sont encore en
-            progression au moment où l'on parle.
-          </p>
-
-          <div className="exemple-section">
-            <div className="lesson-exemple">
-              <span className="title-exemple">Exemple</span>
-              <p className="text">
-                Paul all <span className="wordPrimary">has eaten</span> the cookies
-              </p>
-              <span className="trad-exemple">Paul a mangé tout les cookies</span>
+          <div className="title">
+            <div className="num-primary-block">
+              <span className="num-primary-less">II</span>
             </div>
-            <div className="lesson-exemple">
-              <span className="title-exemple">Exemple</span>
-              <p className="text">
-                Paul all <span className="wordPrimary">has eaten</span> the cookies
-              </p>
-              <span className="trad-exemple">Paul a mangé tout les cookies</span>
-            </div>
+            <p className="title-primary-less"> Les emplois du present perfect et du present Be + -ing  </p>
           </div>
-
-          <p className="explication">
-            Quand l'action a-t-elle commencé ? Il y a dix ans. Est-elle terminée
-            ? Non, Paul habite toujours à Londres. Cela fait 10 ans qu'il y
-            habit
+           <div className="title">
+            <div className="num-block">
+              <span className="num-section-less">A</span>
+            </div>
+            <p className="title-section-less"> Les emplois du present perfect Be - ing </p>
+          </div>
+          <p className="subTitle">Pour faire un bilan d'un résultat d'un évenement passé avec une nuance appréciative 
+                                  (sur le déroulement de l'action, ect)
           </p>
+          <div className="lesson-exemple">
+            <span className="title-exemple">Exemple</span>
+            <p className="text">
+               I  <span className="wordPrimary">have been whasing</span> the dishes
+            </p>
+            <span className="trad-exemple">J'ai fais la vaisselle (cela m'a pris du temps)</span>
+          </div>
+          <div className="lesson-table">
+            <div className="table-text">
+              On peut employer le present perfect be-ing dans le structures habituellement utilisées avec le present 
+              perfect simple si l'on veut ajouter une nuance appréciative sur la durée, le déroulement de l'action ou son
+              impact sur le présent.
+            </div>
+            <div className="table-exemple">
+              <div className="lesson-exemple">
+                <span className="title-exemple">Exemple</span>
+                <p className="text">
+                  I <span className="wordPrimary"> haven't been sleeping </span> a lot lately
+                </p>
+                <span className="trad-exemple">Je n'ai pas beaucoup dormi ces derniers temps</span>
+              </div>
+              <div className="lesson-exemple">
+                <span className="title-exemple">Exemple</span>
+                <p className="text">
+                  I <span className="wordPrimary"> haven't been sleeping </span> a lot lately
+                </p>
+                <span className="trad-exemple">Je n'ai pas beaucoup dormi ces derniers temps</span>
+              </div>
+                <div className="lesson-exemple">
+                  <span className="title-exemple">Exemple</span>
+                  <p className="text">
+                    I <span className="wordPrimary"> haven't been sleeping </span> a lot lately
+                  </p>
+                  <span className="trad-exemple">Je n'ai pas beaucoup dormi ces derniers temps</span>
+                </div>
+                <div className="lesson-exemple">
+                  <span className="title-exemple">Exemple</span>
+                  <p className="text">
+                    I <span className="wordPrimary"> haven't been sleeping </span> a lot lately
+                  </p>
+                  <span className="trad-exemple">Je n'ai pas beaucoup dormi ces derniers temps</span>
+                </div>
+              </div>
+            </div>
+            
+        </>,
+        /*
+         * *
+          4er section
+            **
+              */
+        <>
+           <div className="title">
+            <div className="num-block">
+              <span className="num-section-less">B</span>
+            </div>
+            <p className="title-section-less"> Les emplois du present perfect simple </p>
+          </div>
+          <p className="subTitle">Pour faire un bilan dans le présent d'un événement</p>
+          <div className="lesson-exemple">
+            <span className="title-exemple">Exemple</span>
+            <p className="text">
+              <span className="wordPrimary">Have</span> you <span className="wordPrimary">finished</span> your homework
+            </p>
+            <span className="trad-exemple">Est ce que tu as fini tes devoirs</span>
+          </div>
+          <div className="lesson-table">
+            <div className="table-text">
+              Dans certaines tournures idiomatique, il faut utiliserle present perfect simple
+            </div>
+            <table>
+              <tbody>       
+                <tr>
+                  <td>
+                    <span className="wordPrimary">I have just </span> (+ participe passé)
+                  </td>
+                  <td> Je viens de (+ infinitif)</td>
+                </tr>
+                <tr>
+                  <td>
+                    <span className="wordPrimary">It is the first time </span> (+ present perfect simple))
+                  </td>
+                  <td> Je viens de (+ infinitif)</td>
+                </tr>
+                <tr>
+                  <td>
+                    Present perfect simple  + for (+ durée)
+                  </td>
+                  <td>Présent de l'indicatif + depuis (+ durée)</td>
+                </tr>
+                <tr>
+                <td>
+                  Present perfect + since (+ événement localisé) 
+                </td>
+                <td>Présent de l'indicatif + depuis (+ événement localisé)</td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
         </>
       ],
     },
