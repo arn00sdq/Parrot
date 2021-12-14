ddc272cb377f5725d7fd44bd24c39a5f7125758d
arn00sdq <ad.dudes@outlook.fr>
2021-12-14 18:57:40 +0100

    progress ternary condtion

--- src/components/home/progress/DailyContainer.js
+++ src/components/home/progress/DailyContainer.js
@@ -6,14 +6,19 @@ import {feather_icon,treasure } from '../../../database/images';
 function DailyContainer({progressEx, imgReward, dailyText}){
     console.log(progressEx.count)
     var barProgress = (progressEx.count / progressEx.total) * 100 
+    var missionCompleted = progressEx.count == progressEx.total;
     return (
         <div className="objectif-container">
             <Reward className="reward" type="treasure"/>
             <div className="progress-text-card">{dailyText}</div>
+            {missionCompleted == false 
+            ?   
             <div className="container-bar-xp">
                 <div className="small-text-xp">{progressEx.count} / {progressEx.total}</div>
                 <ProgressBar width="5vw" progress={barProgress+"%"} />
-            </div>   
+            </div> 
+            : <div className="daily-text-finish"> Terminer </div>
+            }           
         </div>
     );
 }
