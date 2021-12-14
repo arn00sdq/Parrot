f4e57633249bc4dec8441eadb7b960bce75567c4
arn00sdq <ad.dudes@outlook.fr>
2021-12-14 14:40:53 +0100

    activePoint W2I

--- src/components/common/displayActivity/exercice/types/word2img.js
+++ src/components/common/displayActivity/exercice/types/word2img.js
@@ -1,36 +1,43 @@
-import React , { useState } from "react";
+import React, { useState } from "react";
 import { achievement, Word2Img } from "../../../../../database/images";
 import ActivityCompleted from "../../ActivityCompleted";
 
-function Word2img({reward, content, currentStep, handles, isFinished,from }) {
+function Word2img({ reward, content, currentStep, handles, isFinished, from }) {
 
   const finished = currentStep < content.steps ? false : true;
   var active = currentStep == content.steps - 1 ? "active" : "";
 
-  let wordImg = []
+  var [getIndex, setIndex] = useState();
+
+  const wordImg = [];
+  let row = [];
 
   const handleButtonClick = (e) => {
+
     e.preventDefault();
     const {
       target: { value },
     } = e;
 
-    if(active == "active"){
-      handles.handleEndActivity(true,"",from);
+    if (active == "active") {
+      handles.handleEndActivity(true, "", from);
     }
     if (value == content.images[currentStep].word && finished == false) {
-      wordImg.filter(word => word == content.images[currentStep].word)
-      handles.handleExerciseNextStep("exercicePage");
+      setIndex(wordImg.findIndex(word => word == value))
+      setTimeout(() => {
+        handles.handleExerciseNextStep("exercicePage");
+      }, 1500);
+
     }
   };
 
-  let row = [];
+  if (finished == false) {
 
-  if (finished == false ) {
-    content.words[currentStep].forEach((word) => {
+    content.words[currentStep].forEach((word, i) => {
+      console.log(word, i)
       wordImg.push(word)
       row.push(
-        
+
         <div className="w2i-btn-container">
           <button
             onClick={(e) => handleButtonClick(e)}
@@ -39,7 +46,7 @@ function Word2img({reward, content, currentStep, handles, isFinished,from }) {
             {word}
           </button>
 
-          <span className={ false == true ? "active-points points-word" : " points-word" }> +1 </span>
+          <span className={i == getIndex ? "active-points points-word" : " points-word"}> +1 </span>
         </div>
 
       );
@@ -47,23 +54,23 @@ function Word2img({reward, content, currentStep, handles, isFinished,from }) {
   }
   return (
     <>
-    <div className="wordImg-container">
+      <div className="wordImg-container">
 
-      <div className="center">
-        <div className="monitor">
-          <img
-            src={finished == false ? content.images[currentStep].url : Word2Img.badge_photo}
-            className="wordImg-picture"
-          />
-          <img className="monitor-logo" src={Word2Img.apple}></img>
+        <div className="center">
+          <div className="monitor">
+            <img
+              src={finished == false ? content.images[currentStep].url : Word2Img.badge_photo}
+              className="wordImg-picture"
+            />
+            <img className="monitor-logo" src={Word2Img.apple}></img>
+          </div>
+          <div className="m-1"></div>
+          <div className="m-2"></div>
         </div>
-        <div className="m-1"></div>
-        <div className="m-2"></div>
-      </div>
 
-      <div className="btn-exercise-container">{row}</div>
-    </div>
-    <ActivityCompleted reward={reward} active={active} isFinished = {isFinished} handles={handles} from={from}/>
+        <div className="btn-exercise-container">{row}</div>
+      </div>
+      <ActivityCompleted reward={reward} active={active} isFinished={isFinished} handles={handles} from={from} />
     </>
   );
 }
